36f9c35cc646f3e5f51dab1ed10e55da8b88cf5f
Andrew Scheff <andrew_scheff@brown.edu>
2011-09-27 22:37:56 -0400

    mongo as dumb datastore for workload now working

--- parser/parse.py
+++ parser/parse.py
@@ -4,11 +4,16 @@ import fileinput
 import hashlib
 import time
 import re
+from pymongo import Connection
 
+###GLOBAL VARS
+current_transaction = None
+mongo_comm = None
 
 INPUT_FILE = "sample.txt"
 
 
+###REGEX STUFF
 TIME_MASK = "[0-9]+\.[0-9]+.*"
 ARROW_MASK = "(-->>|<<--)"
 IP_MASK = "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{5,5}"
@@ -16,7 +21,6 @@ COLLECTION_MASK = "\w+\.\$?\w+"
 SIZE_MASK = "\d+ bytes"
 MAGIC_ID_MASK = "id:\w+ \d+"
 REPLY_ID_MASK = "\d+"
-
 
 HEADER_MASK = "(?P<timestamp>" + TIME_MASK + ") *- *" + \
 "(?P<IP1>" + IP_MASK + ") *" + \
@@ -27,30 +31,79 @@ HEADER_MASK = "(?P<timestamp>" + TIME_MASK + ") *- *" + \
 "(?P<magic_id>" + MAGIC_ID_MASK + ") *" + \
 "-? *(?P<reply_id>" + REPLY_ID_MASK + ")?"
 
+CONTENT_REPLY_MASK = "\s*reply +.*"
+CONTENT_INSERT_MASK = "\s*insert: {.*"
+CONTENT_QUERY_MASK = "\s*query: {.*"
+replyRegex = re.compile(CONTENT_REPLY_MASK)
+insertRegex = re.compile(CONTENT_INSERT_MASK)
+queryRegex = re.compile(CONTENT_QUERY_MASK)
+
+def initDB():
+    global mongo_comm
+    connection = Connection()
+    db = connection.mongo_designer
+    mongo_comm = db.mongo_comm
+    return
+
+
+def store(transaction):
+    global mongo_comm
+    mongo_comm.insert(transaction)
+    return
+
+def process_header_line(header):
+    global current_transaction
+
+    if (current_transaction):
+        store(current_transaction)
+
+    current_transaction = header
+    current_transaction['content'] = []
+    return
+
+def process_content_line(line):
+    global replyRegex
+    global insertRegex
+    global queryRegex
+    global current_transaction
+
+    if (not current_transaction):
+        return
+
+    if (replyRegex.match(line)):
+        current_transaction['type'] = "reply"
+        print("reply")
+    elif (insertRegex.match(line)):
+        current_transaction['type'] = "insert"
+        print("insert")
+    elif (queryRegex.match(line)):
+        current_transaction['type'] = "query"
+        print("query")
+    else:
+        current_transaction['content'].append(line)
+        print("other")
+
+    return
 
 def main():
-    #for line in sys.stdin:
-        #if is_important(line):
-        #    print timestamp(sanitize(line))
-     #   print line,
+    global current_transaction
+
+    initDB()
+
+    header_regex = re.compile(HEADER_MASK);
+
     file = open(INPUT_FILE, 'r')
     line = file.readline()
     while line:
         line = file.readline()
-        if re.match(TIME_MASK, line):
-            print("MATCH")
-            m = re.search(HEADER_MASK, line)
-            if (m):
-                print(m.groupdict())
-            else:
-                print(line)
-                exit()
+        result = header_regex.match(line)
+        if result:
+            process_header_line(result.groupdict())
         else:
-            print("NO.")
-        
-        
+            process_content_line(line)
+
     return
-        
+
 
 if __name__ == '__main__':
 	main()
