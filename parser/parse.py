987a818061d0864790b8a3cd8c211369417db546
Andrew Scheff <Andy@Andrews-MacBook-Pro.local>
2011-10-12 00:10:41 -0400

    unique sessions supported, maybe buggy

--- parser/parse.py
+++ parser/parse.py
@@ -12,6 +12,9 @@ from pymongo import Connection
 current_transaction = None
 mongo_comm = None
 
+current_session_map = {}
+session_uid = 100
+
 INPUT_FILE = "sample.txt"
 
 TIME_MASK = "[0-9]+\.[0-9]+.*"
@@ -35,6 +38,8 @@ headerRegex = re.compile(HEADER_MASK);
 CONTENT_REPLY_MASK = "\s*reply +.*"
 CONTENT_INSERT_MASK = "\s*insert: {.*"
 CONTENT_QUERY_MASK = "\s*query: {.*"
+
+
 replyRegex = re.compile(CONTENT_REPLY_MASK)
 insertRegex = re.compile(CONTENT_INSERT_MASK)
 queryRegex = re.compile(CONTENT_QUERY_MASK)
@@ -46,9 +51,21 @@ def initDB(hostname, port):
     mongo_comm = db.mongo_comm
     return
 
-
 def store(transaction):
     global mongo_comm
+    global current_session_map
+    global session_uid
+
+    if (current_transaction['arrow'] == '-->>'):
+        ip = current_transaction['IP1']
+    else:
+        ip = current_transaction['IP2']
+
+    if (ip not in current_session_map):
+        current_session_map[ip] = session_uid
+        session_uid += 1
+
+    current_transaction['uid'] = current_session_map[ip]
     mongo_comm.insert(transaction)
     return
 
@@ -67,24 +84,30 @@ def process_content_line(line):
     global insertRegex
     global queryRegex
     global current_transaction
+    global current_session_map
+    global session_uid
 
     if (not current_transaction):
         return
 
     if (replyRegex.match(line)):
         current_transaction['type'] = "reply"
-        print("reply")
+
     elif (insertRegex.match(line)):
         current_transaction['type'] = "insert"
-        print("insert")
+	line = line[line.find('{'):line.rfind('}')+1]
+
     elif (queryRegex.match(line)):
         current_transaction['type'] = "query"
-        print("query")
-    else:
-	obj = yaml.load(line)
-	print(obj)
-        current_transaction['content'].append(obj)
-        print("other")
+	line = line[line.find('{'):line.rfind('}')+1]
+
+    obj = yaml.load(line)
+
+    if ('whatismyuri' in obj):
+	current_session_map[current_transaction['IP1']] = session_uid
+	session_uid += 1        
+
+    current_transaction['content'].append(obj)
 
     return
 
@@ -108,10 +131,14 @@ def main():
     while line:
         line = file.readline()
         result = headerRegex.match(line)
+	print line
         if result:
             process_header_line(result.groupdict())
         else:
             process_content_line(line)
+
+    if (current_transaction):
+        store(current_transaction)
 
     return
 
