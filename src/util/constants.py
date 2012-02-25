c0d5f040d076448a5c0bdb9a1eaf3372eebf2d77
Christopher Keith <cleophus1998@gmail.com>
2012-02-25 10:04:18 -0500

    - added new project contents for collections in the Metadata DB

--- src/util/constants.py
+++ src/util/constants.py
@@ -3,6 +3,10 @@
 PROJECT_NAME = "MongoDB-Designer"
 PROJECT_URL = "https://github.com/apavlo/MongoDB-Designer"
 
+# Metadata DB Collections
+COLLECTION_SCHEMA = "schema"
+COLLECTION_WORKLOAD = "sessions"
+
 CATALOG_COLL = "catalog"
 CATALOG_FIELDS = 'fields'
 
