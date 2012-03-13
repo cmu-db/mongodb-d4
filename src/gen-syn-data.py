78e6873dd422de30e799e30a3840c2d8c1ca9857
Christopher Keith <cleophus1998@gmail.com>
2012-03-13 17:31:11 -0400

    - initial construction of framework for generating synthetic data for micro-benchmarking

---
+++ src/gen-syn-data.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import argparse
+import logging
+import pymongo
+import mongokit
+from pprint import pprint
+from ConfigParser import SafeConfigParser
+
+import catalog
+import workload
+import search
+import random
+from util import *
+
+LOG = logging.getLogger(__name__)
+
+## ==============================================
+## main
+## ==============================================
+if __name__ == '__main__':
+    
+    aparser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+                                      description="%s\n%s" % (constants.PROJECT_NAME, constants.PROJECT_URL))
+    aparser.add_argument('--config', type=file,
+                         help='Path to %s configuration file' % constants.PROJECT_NAME)
+    aparser.add_argument('--host', type=str, default="localhost",
+                         help='The hostname of the MongoDB instance containing the sample workload')
+    aparser.add_argument('--print-config', action='store_true',
+                         help='Print out the default configuration file used by %s' % constants.PROJECT_NAME)
+    aparser.add_argument('--reset', action='store_true', help='Reset collection statistics')
+    aparser.add_argument('--debug', action='store_true',
+                         help='Enable debug log messages')
+    args = vars(aparser.parse_args())
+
+    if args['debug']: logging.getLogger().setLevel(logging.DEBUG)
+    if args['print_config']:
+        print config.makeDefaultConfig()
+        sys.exit(0)
+    
+    if not args['config']:
+        logging.error("Missing configuration file")
+        print
+        aparser.print_help()
+        sys.exit(1)
+    logging.debug("Loading configuration file '%s'" % args['config'])
+    cparser = SafeConfigParser()
+    cparser.read(os.path.realpath(args['config'].name))
+    config.setDefaultValues(cparser)
+    
+    ## ----------------------------------------------
+    ## Connect to MongoDB
+    ## ----------------------------------------------
+    hostname = cparser.get(config.SECT_MONGODB, 'hostname')
+    port = cparser.getint(config.SECT_MONGODB, 'port')
+    assert hostname
+    assert port
+    try:
+        conn = mongokit.Connection(host=hostname, port=port)
+    except:
+        LOG.error("Failed to connect to MongoDB at %s:%s" % (hostname, port))
+        raise
+    ## Register our objects with MongoKit
+    conn.register([ catalog.Collection, workload.Session ])
+
+    ## FOR
+    generate_db = conn['synthetic']
+    
+    ## -----------------------------------------------------
+    ## Generate Synthetic Data for Micro-Benchmarking
+    ## -----------------------------------------------------
+    print 'Begin generating synthetic data'
+    
+    for i in range(100) :
+        doc = {}
+        doc['key1']  = i
+        doc['key2'] = i * i
+        doc['key3'] = 'help'
+    	generate_db['test'].insert(doc)
+## END MAIN
