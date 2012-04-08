b78efd121ab218e45e801db17ff92cd3a8094850
Christopher Keith <cleophus1998@gmail.com>
2012-04-08 12:03:46 -0400

    - added unit test repository for cost model functions

---
+++ src/cost-model-unit-tests.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+import costmodel
+import search
+import workload
+
+class TestCostModel (unittest.TestCase) :
+    
+    def setUp(self) :
+        pass
+        
+    def testInitialization(self) :
+        self.assertEqual(True, False)
+        
+## End Class
+
+if __name__ == '__main__':
+    unittest.main()
+## END MAIN
