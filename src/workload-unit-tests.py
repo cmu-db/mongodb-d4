1d89f55f2cbcfcf0f41bd34b8b18f7485d918ced
Christopher Keith <cleophus1998@gmail.com>
2012-04-08 15:41:37 -0400

    - added methods and tests for adding a session(s) to a workload instance

--- src/workload-unit-tests.py
+++ src/workload-unit-tests.py
@@ -8,7 +8,19 @@ class TestWorkload (unittest.TestCase) :
     
     def setUp(self) :
         pass
-        
+    
+    def testAddSession(self) :
+        session = {}
+        w = workload.Workload()
+        w.addSession(session)
+        self.assertEqual(w.sessions, [session])
+    
+    def testAddSessions(self) :
+        sessions = [{}, {}]
+        w = workload.Workload()
+        w.addSessions(sessions)
+        self.assertEqual(w.sessions, sessions)
+    
     def testWorkloadFactory(self) :
         w = workload.Workload.testFactory()
         self.assertEqual(isinstance(w, workload.Workload), True)
--- src/workload/workload.py
+++ src/workload/workload.py
@@ -3,8 +3,15 @@
 class Workload(object) :
 
     def __init__(self) :
-        pass
+        self.sessions = []
         
+    def addSession(self, session) :
+        self.sessions.append(session)
+    
+    def addSessions(self, sessions) :
+        for session in sessions :
+            self.addSession(session)
+    
     @staticmethod
     def testFactory() :
         return Workload()
