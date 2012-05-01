abcd57170ffec1d33562561b32ec8a24509ce3d0
Andy Pavlo <pavlo@cs.brown.edu>
2012-05-01 10:51:37 -0400

    Moved utility code out of 'gather-stats' into workload.StatsProcessor

--- src/workload/__init__.py
+++ src/workload/__init__.py
@@ -5,7 +5,9 @@ from traces import Session
 from workload import Workload
 from session import Sess
 from query import Query
+from stats import StatsProcessor
 
 del utilmethods
 del traces
 del workload
+del stats
