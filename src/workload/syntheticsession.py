ec974ab605d7c5d237bf91406f63d9dbed88f290
Christopher Keith <cleophus1998@gmail.com>
2012-05-02 07:21:27 -0400

    - added comments to SyntheticSession class

--- src/workload/syntheticsession.py
+++ src/workload/syntheticsession.py
@@ -1,5 +1,35 @@
 # -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------
+# Copyright (C) 2012
+# Andy Pavlo - http://www.cs.brown.edu/~pavlo/
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+# -----------------------------------------------------------------------
 
+'''
+This class is an abstraction of the actual workload sessions parsed either via the 
+MongoSniff or MySQL data extract.  It is not much more than an ordered list of queries 
+that exist in the workload (instances of the Query class) and the start and end times
+for the session.  The start and end times are important for helping to calculate the 
+skew cost in the cost model.
+'''
 class SyntheticSession(object) :
 
     def __init__(self) :
