c2c0da00239607960b864d2a4c740280b3e2938e
Christopher Keith <cleophus1998@gmail.com>
2012-05-08 09:16:13 -0400

    - finished evaluation tests for Network Cost function

--- src/eval-cm.py
+++ src/eval-cm.py
@@ -39,7 +39,17 @@ if __name__ == '__main__':
                     'query_use_count' : 1000,
                     'cardinality' : 250,
                     'selectivity' : 0.25,
-                }
+                },
+                'col4' : {
+                    'query_use_count' : 500,
+                    'cardinality' : 100,
+                    'selectivity' : 1.0,
+                },
+                'col5' : {
+                    'query_use_count' : 500,
+                    'cardinality' : 100,
+                    'selectivity' : 1.0,
+                },
             },
             'tuple_count' : 100000,
             'workload_queries' : 1000,
@@ -66,9 +76,9 @@ if __name__ == '__main__':
         q.collection = 'A'
         q.type = 'select'
         if i % 2 == 1 :
-            q.predicates = {'col1' : 'equality', 'col2' : 'equality'}
+            q.predicates = {'col1' : 'equality', 'col2' : 'equality', 'col3' : 'range'}
         else :
-            q.predicates = {'col1' : 'equality', 'col2' : 'range'}
+            q.predicates = {'col1' : 'equality', 'col2' : 'range', 'col3' : 'range', 'col4' : 'range'}
         q.projection = {}
         q.timestamp = ts + i
         sess.queries.append(q)
@@ -88,11 +98,23 @@ if __name__ == '__main__':
     d2.addShardKey('A', ['col2'])
     print cm.networkCost(d2)
     
-    print '** TEST 3: All ops executed at 4 shards '
+    print '** TEST 3: All ops executed at 2 shards '
     d3 = search.Design()
     d3.addCollection('A')
     d3.addShardKey('A', ['col3'])
     print cm.networkCost(d3)
+    
+    print '** TEST 4: All ops executed at 2 shards '
+    d4 = search.Design()
+    d4.addCollection('A')
+    d4.addShardKey('A', ['col4'])
+    print cm.networkCost(d4)
+    
+    print '** TEST 5: All ops executed at 2 shards '
+    d5 = search.Design()
+    d5.addCollection('A')
+    d5.addShardKey('A', ['col5'])
+    print cm.networkCost(d5)
     
     ## ----------------------------------------------
     ## STEP 2
