f98140790c36775796ca7bc14a5312ae21a88d41
Emanuel Buzek <emanuel.buzek@gmail.com>
2012-05-24 16:39:15 -0400

    sanitizer unit test

--- src/sanitizer/anonymize.py
+++ src/sanitizer/anonymize.py
@@ -7,69 +7,126 @@ import time
 import re
 import optparse
 
-def find_quote(line, startIndex):
-    index = startIndex
-    while index < len(line):
-        index = line.find("\"", index)
-        if index < 0:
-            return (-1, False)
-        if (index == 0) or (line[index-1] != "\\"):
-            # found an unescaped quote
-            break
-        # advance after the current "
-        index += 1
-    isKey = False
-    if len(line)-index > 2:
-        isKey = line[index + 2] == ":"
-    return (index, isKey)
 
-def hash_string(s, salt):
-    return "%s/%d" % (hashlib.md5(str(salt) + s).hexdigest(), len(s))
-
-def sanitize(line, salt):
-    startIndex = 0
-    endIndex = 0
-    resultLine = ""
-    #print line
+class Sanitizer:
     
-    # go through the line, and find unescaped quotes
-    # replace strings with their hash value
-    while endIndex < len(line):
-        # find start of a string
-        (startIndex, flag) = find_quote(line, endIndex)
-        if startIndex < 0:
-            break
-        
-        # store the substring between the last quote and the current one    
-        b = line[endIndex: startIndex]
-        
-        # find the end of the string
-        (endIndex, isKey) = find_quote(line, startIndex + 1)
-        if endIndex < 0:
-            #print "ERROR: open string: ", line
-            # on error, just don't sanitize...
-            return line
-        endIndex += 1
-        
-        # this is the substring we want to hash
-        string = line[startIndex: endIndex]
-        #print "found string: ", string
-        if not isKey:
-            string = hash_string(string[1:len(string)-1], salt) #strip surrounding quotes
-        # append to the result...
-        resultLine = resultLine + b + string
-    ### END WHILE
-    
-    # append the rest of the line
-    if endIndex < len(line):
-        resultLine = resultLine + line[endIndex: len(line)]
-    #print "RESULT:", resultLine
-    return resultLine.rstrip("\n")
 
-#this selects only lines we care about - starting with query
-def is_important(line):
-    l = line.lstrip("\t ") #tab and space
-    return l.startswith("query")
+    def find_quote(self, line, startIndex):
+        index = startIndex
+        while index < len(line):
+            index = line.find("\"", index)
+            if index < 0:
+                return (-1, False)
+            if (index == 0) or (line[index-1] != "\\"):
+                # found an unescaped quote
+                break
+            # advance after the current "
+            index += 1
+        isKey = False
+        if len(line)-index > 2:
+            isKey = line[index + 2] == ":"
+        return (index, isKey)
+    
+    def hash_string(self, s, salt):
+        #print s
+        hash = hashlib.md5(str(salt) + s).hexdigest()
+        if self.test:
+            hash = "XXX_HASH_XXX"
+        output = "%s/%d" % (hash, len(s))
+        return output
+        
+    def sanitize(self, line, salt):
+        startIndex = 0
+        endIndex = 0
+        resultLine = ""
+        #print line
+        
+        # go through the line, and find unescaped quotes
+        # replace strings with their hash value
+        while endIndex < len(line):
+            # find start of a string
+            (startIndex, flag) = self.find_quote(line, endIndex)
+            if startIndex < 0:
+                break
+            
+            # store the substring between the last quote and the current one    
+            b = line[endIndex: startIndex]
+            
+            # find the end of the string
+            (endIndex, isKey) = self.find_quote(line, startIndex + 1)
+            if endIndex < 0:
+                #print "ERROR: open string: ", line
+                # on error, just don't sanitize...
+                return line
+            endIndex += 1
+            
+            # this is the substring we want to hash
+            string = line[startIndex: endIndex]
+            #print "found string: ", string
+            if not isKey:
+                string = self.hash_string(string[1:len(string)-1], salt) #strip surrounding quotes
+            # append to the result...
+            resultLine = resultLine + b + string
+        ### END WHILE
+        
+        # append the rest of the line
+        if endIndex < len(line):
+            resultLine = resultLine + line[endIndex: len(line)]
+        #print "RESULT:", resultLine
+        return resultLine.rstrip("\n")
+    
+    #this selects only lines we care about - starting with query
+    def is_important(self, line):
+        l = line.lstrip("\t ") #tab and space
+        return l.startswith("query")
+    
+    
+    def process_line(self, line):
+        line = self.sanitize(line, self.salt)
+            
+        # Check whether this is a new command
+        # If it is, then this is only line that should get a timestamp
+        if self.new_command.match(line):
+            if not self.test:
+                timestamp = repr(time.time()) + " -"
+            else:
+                timestamp =  "000.000 -"
+        else:
+            timestamp = "  "
+        
+        output =  "%-20s %s" % (timestamp, line)
+        
+        return output
+
+
+    def start(self):
+        line = sys.stdin.readline()
+        
+        while line:
+            
+            output = self.process_line(line, newCommand)
+            
+            print output
+            if f:
+                f.write(output)
+                f.write("\n")
+            line = sys.stdin.readline()
+    
+        f.close()
+
+    def __init__(self, options, args, test):
+        self.f = None 
+        self.new_command = re.compile("^(.*?) (\-\->>|<<\-\-) (.*?)")
+        if options:
+            self.salt = options.salt        
+            if options.out: 
+                self.f = open(options.out, 'w')
+        else:
+            self.salt = 0
+        self.test = test
+
+        
+
 
 if __name__ == '__main__':
     aparser = optparse.OptionParser(description='MongoSniff Trace Anonymizer')
@@ -79,30 +136,9 @@ if __name__ == '__main__':
                          help='output file name')
     (options, args) = aparser.parse_args()
     
-    f = None 
-    if options.out: 
-        f = open(options.out, 'w')
-
-    newCommand = re.compile("^(.*?) (\-\->>|<<\-\-) (.*?)")
-    line = sys.stdin.readline()
-    while line:
-        line = sanitize(line, options.salt)
-        
-        # Check whether this is a new command
-        # If it is, then this is only line that should get a timestamp
-        if newCommand.match(line):
-            timestamp = repr(time.time()) + " -"
-        else:
-            timestamp = "  "
-        
-        output =  "%-20s %s" % (timestamp, line)
-        print output
-        if f:
-            f.write(output)
-            f.write("\n")
-        line = sys.stdin.readline()
-
-    f.close()
+    s = Sanitizer(options, args, False) # test: false
+    s.start()
+    
 ## MAIN        
 
 
--- tests/sanitizer/unittest_sanitizer.py
+++ tests/sanitizer/unittest_sanitizer.py
@@ -20,26 +20,27 @@ def get_long_string(strings, iters):
 class TestSanitizer (unittest.TestCase):
     
     def setUp(self):
+        print("\n\n === Sanitizer Test === \n")
         pass
     
     
     
     def testHashString(self):
     
-        print("\n\n === Sanitizer Test === \n")
-    
+        s = anonymize.Sanitizer(None, None, True)
+        
         # short strings
         short_string1 = get_long_string([' \\"hello\\" ', '\n\n \t\t \n\t \t\n', 'email: \\"emanuel@buzek.net\\"', '\'\'\'\'', "'\\\"'", "{inside: \\\"of a string\\\"}"], 1)
         short_string2 = get_long_string(['This is not a long string.  \n\n \t', '\n\t', '{\\"name\\"}', ' \\ END', '\\"string\\"', "'string'", "\\\"'quotes'\\\"", "open quotes: \\\" '"], 1)
         
-        hash1 = anonymize.hash_string(short_string1, 0)
-        hash2 = anonymize.hash_string(short_string2, 0)
+        hash1 = s.hash_string(short_string1, 0)
+        hash2 = s.hash_string(short_string2, 0)
         
         json = '{"key1" : "%s", "key2" : "%s"}' % (short_string1, short_string2)
         print json 
         
         expected_sanitized_json = '{"key1" : %s, "key2" : %s}' % (hash1, hash2)
-        real_sanitized_json = anonymize.sanitize(json, 0)
+        real_sanitized_json = s.sanitize(json, 0)
         
         self.assertEqual(expected_sanitized_json, real_sanitized_json)
     
@@ -49,13 +50,13 @@ class TestSanitizer (unittest.TestCase):
         long_string1 = get_long_string([' \\"hello\\" ', '\n\n \t\t \n\t \t\n', 'email: \\"emanuel@buzek.net\\"', '\'\'\'\'', "'\\\"'", "{inside: \\\"of a string\\\"}"], 5000)
         long_string2 = get_long_string(['This is a very string.  \n\n \t', '\n\t', '{\\"name\\"}', ' \\ END', '\\"string\\"', "'string'", "\\\"'quotes'\\\"", "open quotes: \\\" '"], 5000)
         
-        hash1 = anonymize.hash_string(long_string1, 0)
-        hash2 = anonymize.hash_string(long_string2, 0)
+        hash1 = s.hash_string(long_string1, 0)
+        hash2 = s.hash_string(long_string2, 0)
         
         long_json = '{"key1" : "%s", "key2" : "%s"}' % (long_string1, long_string2)
         
         expected_sanitized_long_json = '{"key1" : %s, "key2" : %s}' % (hash1, hash2)
-        real_sanitized_long_json = anonymize.sanitize(long_json, 0)
+        real_sanitized_long_json = s.sanitize(long_json, 0)
         
         self.assertEqual(expected_sanitized_long_json, real_sanitized_long_json)
     
@@ -63,8 +64,8 @@ class TestSanitizer (unittest.TestCase):
         # other tests
         
         str1 = "\"THIS SHOULD BE SIMPLY HASHED\""
-        hash1 = anonymize.hash_string("THIS SHOULD BE SIMPLY HASHED", 0)
-        result1 = anonymize.sanitize(str1, 0)
+        hash1 = s.hash_string("THIS SHOULD BE SIMPLY HASHED", 0)
+        result1 = s.sanitize(str1, 0)
         self.assertEqual(hash1, result1)
         
         
@@ -72,10 +73,10 @@ class TestSanitizer (unittest.TestCase):
         
         
         text = 'string with \\\"escaped quotes\\\"'
-        hashed_text = anonymize.hash_string(text, 0)
+        hashed_text = s.hash_string(text, 0)
         long_json = "{" + get_long_string(['"key" : "%s", ' % text], 4000) + "}"
         expected_result = "{" + get_long_string(['"key" : %s, ' % hashed_text], 4000) + "}"
-        real_result = anonymize.sanitize(long_json, 0)
+        real_result = s.sanitize(long_json, 0)
         
         #print long_json
         #print expected_result
@@ -83,8 +84,29 @@ class TestSanitizer (unittest.TestCase):
         
         self.assertEqual(expected_result, real_result)
         
-        print "DONE"
+        print("done")
+
+    def testTrace(self):
         
+        # This test loads two traces - a clean, unhashed trace form the sniffer, and the anonymized trace
+        # The clean trace is then anonymized and compared to the expected output.
+        # This verifies that the sanitizer works as expected.
+        
+        clean_lines = open("trace-clean.out", "r").readlines()
+        anon_lines = open("trace-anon.out", "r").readlines()
+
+        
+        s = anonymize.Sanitizer(None, None, True) # test: True
+
+        for i in range(len(clean_lines)):
+            source = clean_lines[i]
+            expected = anon_lines[i]
+            result = s.process_line(source) + "\n"
+            #print result
+            #print expected
+            self.assertEqual(expected, result)
+
+        print "done"
 
 ## CLASS
 
