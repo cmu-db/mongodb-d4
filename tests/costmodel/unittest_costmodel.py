c3e4e0150ff667bdc3056c7840240f8070359430
Andy Pavlo <pavlo@cs.brown.edu>
2012-06-25 00:36:13 -0400

    This hopefully fixes the dependency problems on Jenkins

--- tests/costmodel/unittest_costmodel.py
+++ tests/costmodel/unittest_costmodel.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os, sys
+basedir = os.path.realpath(os.path.dirname(__file__))
+sys.path.append(os.path.join(basedir, "../../src"))
+
 import unittest
 import costmodel
 import search
