323327ca6d9957c7b718374e4d5c04ab35006308
Andy Pavlo <pavlo@cs.brown.edu>
2012-06-24 20:09:40 -0400

    More refactoring. Trying to connect all of the pieces together

---
+++ src/parser/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# Third-Party Dependencies
+import os, sys
+basedir = os.path.realpath(os.path.dirname(__file__))
+sys.path.append(os.path.join(basedir, "../../libs"))
+
+from mongosniffconvertor import MongoSniffConvertor
