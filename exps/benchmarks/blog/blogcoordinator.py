beb71f4df2352fdbdc67ed3c4968f46813ccd244
Michail Michailidis <mmichaelid@gmail.com>
2012-09-16 22:40:39 -0400

    Added the config option of database name to be used for resetting/loading/benchmarks

--- exps/benchmarks/blog/blogcoordinator.py
+++ exps/benchmarks/blog/blogcoordinator.py
@@ -101,7 +101,7 @@ class BlogCoordinator(AbstractCoordinator):
         
         if LOG.isEnabledFor(logging.DEBUG):
             LOG.debug("# of Articles:   %d" % self.num_articles)
-            LOG.debug("Experiment Type: %s" % config[self.name]["experiment"])
+            LOG.debug("Experiment Type: %s" % config[self.name]["experiment"]) 
             LOG.debug("Sharding Type:   %s" % config[self.name]["sharding"])
             LOG.debug("Denormalize:     %s" % config[self.name]["denormalize"])
             LOG.debug("Indexing Type:   %s" % config[self.name]["indexes"])
