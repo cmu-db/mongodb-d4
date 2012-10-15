458f55659c033c3763a2cf1ec6f4ef022ee90132
Michail Michailidis <mmichaelid@gmail.com>
2012-10-14 22:00:29 -0400

    minor fix at zipfian generator

--- exps/benchmarks/blog/util/zipf.py
+++ exps/benchmarks/blog/util/zipf.py
@@ -14,8 +14,8 @@ class ZipfGenerator:
     def __init__(self, n, alpha = 1.001): 
         
         self.ndarray=np.random.zipf(alpha, n)
-        self.currentpos = 0;
-        self.num = n;
+        self.currentpos = 0
+        self.num = n
         # Calculate Zeta values from 1 to n: 
         #tmp = [1. / (math.pow(float(i), alpha)) for i in range(1, n+1)] 
         #zeta = reduce(lambda sums, x: sums + [sums[-1] + x], tmp, [0]) 
@@ -29,7 +29,7 @@ class ZipfGenerator:
         
         if currentpos < self.num:
             tobereturned = ndarray[self.currentpos]
-            self.currentpos += 1;
+            self.currentpos += 1
             return ndarray[self.currentpos]
         # Take a uniform 0-1 pseudo-random value: 
         # u = random.random()  
