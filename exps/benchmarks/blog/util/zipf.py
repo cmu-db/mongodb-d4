40c8ab7736690ffb5675aa85b0ac1d930eacfbef
Michail Michailidis <mmichaelid@gmail.com>
2012-10-14 22:54:56 -0400

    zipfian generator update

--- exps/benchmarks/blog/util/zipf.py
+++ exps/benchmarks/blog/util/zipf.py
@@ -11,31 +11,16 @@ import numpy as np
 ## -----------------------------------------------------
 class ZipfGenerator: 
     
-    def __init__(self, n, alpha = 1.001): 
-        
-        self.ndarray=np.random.zipf(alpha, n)
-        self.currentpos = 0
-        self.num = n
-        # Calculate Zeta values from 1 to n: 
-        #tmp = [1. / (math.pow(float(i), alpha)) for i in range(1, n+1)] 
-        #zeta = reduce(lambda sums, x: sums + [sums[-1] + x], tmp, [0]) 
-        
-        
-        
-        # Store the translation map: 
-        #self.distMap = [x / zeta[-1] for x in zeta] 
+    def __init__(self, n, alpha = 1.001):  
+        if alpha <= 1.000:
+            alpha = 1.001
+        self.num = n #expected returned numbers 0...31 (e.g for n=32 authors)
     
     def next(self): 
-        
-        if currentpos < self.num:
-            tobereturned = ndarray[self.currentpos]
-            self.currentpos += 1
-            return ndarray[self.currentpos]
-        # Take a uniform 0-1 pseudo-random value: 
-        # u = random.random()  
-        
-        # Translate the Zipf variable: 
-        #return bisect.bisect(self.distMap, u) - 1
-
-        #return i - 1 
+        while 1:
+            tobereturned = np.random.zipf(alpha)
+            if tobereturned  <= self.num
+                break;
+        return tobereturned - 1;
 ## CLASS
+
