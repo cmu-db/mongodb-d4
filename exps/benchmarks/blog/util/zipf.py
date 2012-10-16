6d5563d596a6bcf9ae24aa6b584f790bcf61e4f7
Michail Michailidis <mmichaelid@gmail.com>
2012-10-16 11:10:59 -0400

    zipfian minor fix

--- exps/benchmarks/blog/util/zipf.py
+++ exps/benchmarks/blog/util/zipf.py
@@ -13,12 +13,14 @@ class ZipfGenerator:
     
     def __init__(self, n, alpha = 1.001):  
         if alpha <= 1.000:
-            alpha = 1.001
+            self.alph = 1.001
+        elif: 
+            self.alph = alpha
         self.num = n #expected returned numbers 0...31 (e.g for n=32 authors)
     
     def next(self): 
         while 1:
-            tobereturned = np.random.zipf(alpha)
+            tobereturned = np.random.zipf(self.alph)
             if tobereturned  <= self.num:
                 break
         return tobereturned - 1;
